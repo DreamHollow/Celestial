a0b763c68a248179eb1d75be91ba6c57339f98c5
DreamHollow <404errorman@gmail.com>
2023-02-25 17:12:11 -0500

    Application.hpp: Commit

---
+++ include/Application.hpp
@@ -0,0 +1,19 @@
+#ifndef APPLICATION_HPP
+#define APPLICATION_HPP
+
+class Application
+{
+public:
+    Application();
+    virtual ~Application();
+
+private:
+    // Variables
+
+    // Functions
+
+    // Objects
+    
+};
+
+#endif

0f752f8926e442a66817cb2c610e51a914d4811d
DreamHollow <404errorman@gmail.com>
2023-02-26 11:28:38 -0500

    Application.hpp: Created SFML vars

--- include/Application.hpp
+++ include/Application.hpp
@@ -1,6 +1,8 @@
 #ifndef APPLICATION_HPP
 #define APPLICATION_HPP
+#include <string>
 #include "Globals.h"
+
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
@@ -11,13 +13,37 @@ public:
     Application();
     virtual ~Application();
 
+    // Public Functions
+    void run();
+
 private:
     // Variables
+    float delta_time;
+    unsigned int width;
+    unsigned int height;
+    unsigned int bits;
+    unsigned int framerate;
 
-    // Functions
+    // Init
+    void init_vars();
+    void init_window(unsigned int w, unsigned int h, unsigned int b);
+
+    // Memory
+    void free();
+
+    // Update
+    void update(const float& dt);
+    void update_dt();
+    void update_events();
+
+    // Render
+    void render();
 
     // Objects
-    
+    sf::VideoMode vid_mode;
+    sf::Event w_event;
+    sf::RenderWindow* window;
+    sf::Clock clock;
 };
 
 #endif
